22eb44187b3b3379167a38bed20b78a4182ca570
Lola Dolinsky <dolinsky.lola@gmail.com>
2020-08-22 22:39:48 -0600

    warm-up.js, book-test.js, shelf-test.js complete

--- src/book.js
+++ src/book.js
@@ -1,12 +1,48 @@
-function createTitle() {
-  
+
+function createTitle(bookIdea) {
+  var modified = "The " + bookIdea
+  return modified
 }
+
+function buildMainCharacter(name, age, pronouns) {
+  var ghoulCharacter = {}
+  ghoulCharacter.name = name
+  ghoulCharacter.age = age
+  ghoulCharacter.pronouns = pronouns
+  return ghoulCharacter
+}
+
+function saveReview(review, reviews) {
+  if (!reviews.includes(review)) {
+    reviews.push(review)
+  }
+  return reviews
+}
+
+function calculatePageCount(bookTitle) {
+  return bookTitle.length * 20
+}
+
+function writeBook(bookTitle, bookCharacter, pageCount, genre) {
+  var genre = 'fantasy'
+  var book = {}
+  book.title = bookTitle
+  book.mainCharacter = bookCharacter
+  book.pageCount = calculatePageCount(bookTitle)
+  book.genre = genre
+  return book
+}
+
+function editBook(ghoulBook) {
+    ghoulBook.pageCount = calculatePageCount(ghoulBook.title) * .75;
+    return ghoulBook
+  }
 
 module.exports = {
   createTitle: createTitle,
-  // buildMainCharacter: buildMainCharacter,
-  // saveReview: saveReview,
-  // calculatePageCount: calculatePageCount,
-  // writeBook: writeBook,
-  // editBook: editBook
+  buildMainCharacter: buildMainCharacter,
+  saveReview: saveReview,
+  calculatePageCount: calculatePageCount,
+  writeBook: writeBook,
+  editBook: editBook
 }
--- src/shelf.js
+++ src/shelf.js
@@ -1,8 +1,40 @@
+function shelfBook(book, shelf) {
+  if (shelf.length < 3) {
+    shelf.unshift(book)
+  }
+  return shelf
+}
 
+function unshelfBook(title, shelf) {
+  for (var i = shelf.length - 1; i >= 0; --i) {
+      if (shelf[i].title == title) {
+          shelf.splice(i,1);
+      }
+  }
+  return shelf
+}
+
+function listTitles(shelf) {
+  var list = []
+  for (var i = shelf.length - 1; i >= 0; --i) {
+      list.unshift(shelf[i].title.toString())
+  }
+  var stringList = list.join(", ")
+  return stringList
+}
+
+function searchShelf(shelf, title) {
+  for (var i = shelf.length - 1; i >= 0; --i) {
+    if (shelf[i].title == title) {
+        return true;
+    }
+  }
+  return false
+}
 
 module.exports = {
-  // shelfBook: shelfBook,
-  // unshelfBook: unshelfBook,
-  // listTitles: listTitles,
-  // searchShelf: searchShelf
+  shelfBook: shelfBook,
+  unshelfBook: unshelfBook,
+  listTitles: listTitles,
+  searchShelf: searchShelf
 };
