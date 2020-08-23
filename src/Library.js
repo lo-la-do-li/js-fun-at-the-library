372990e6acdfd2d92ba08e594e94f7e82187131f
Lola Dolinsky <dolinsky.lola@gmail.com>
2020-08-22 23:42:23 -0600

    completed up to addBook function

--- src/Library.js
+++ src/Library.js
@@ -1,7 +1,25 @@
+function createLibrary(name) {
+  var library = {}
+  library.name = name
+  library.shelves = {fantasy: [], fiction: [], nonFiction: []}
+  return library
+}
 
+function addBook(library, book) {
+library.shelves.fantasy.push(book)
+  var shelvesArray = Object.keys(library.shelves)
+  for (var i = shelvesArray.length - 1; i >= 0; --i) {
+    if (shelvesArray[i] == book.genre) {
+        console.log(shelvesArray[i]);
+        library.shelves[shelvesArray[i]].push(book)
+    }
+  }
+  // console.log(shelvesArray)
+  return library
+}
 
 module.exports = {
-  // createLibrary: createLibrary,
-  // addBook: addBook,
+  createLibrary: createLibrary,
+  addBook: addBook,
   // checkoutBook: checkoutBook
 };
